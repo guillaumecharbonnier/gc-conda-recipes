1423a4a0b2272a29da6c2c9fef729710e013f5f6
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2022-09-26 18:18:42 +0200

    add untested octopus recipe

---
+++ recipes/octopus/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+export CPATH=${PREFIX}/include
+export CMAKE_LDFLAGS="-L${PREFIX}/lib"
+export LIBRARY_PATH=${PREFIX}/lib
+
+scripts/install.py \
+	-c ${CC_FOR_BUILD} \
+	-cxx ${CXX_FOR_BUILD} \
+	--prefix ${PREFIX}/bin \
+	--gmp ${PREFIX} \
+	--boost ${PREFIX} \
+	--htslib ${PREFIX} \
+	--threads 1 \
+	--verbose
